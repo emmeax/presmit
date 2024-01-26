195c9d4e727c605cb81802d27316c1f9593ef8f7
Badmus Emmanuel <badmusemmanuel77@gmail.com>
2024-01-26 12:29:52 +0100

    Emmeax

---
+++ prestmit/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import HeroSection from './components/HeroSection'
+import Ourservices from './components/Ourservices'
+import Stats from './components/Stats'
+import Growth from './components/Growth'
+import Testimonials from './components/Testimonials'
+import FAQs from './components/FAQs'
+import Blog from './components/Blog'
+import Newsletter from './components/Newsletter'
+import Footer from './components/Footer'
+
+function App() {
+  
+
+  return (
+  
+      <div>
+       <HeroSection/>
+       <Ourservices/>
+       <Stats/>
+       <Growth/>
+       <Testimonials/>
+       <FAQs/>
+       <Blog/>
+       <Newsletter/>
+       <Footer/>
+    </div>
+      
+    
+  )
+}
+
+export default App
